854bdd7ba5a34b18aac1bdbb9c0d73ad5562d6ac
schasse <mail@sebastianschasse.de>
2017-09-02 11:48:59 +0200

    refactor RepoFetcher

--- lib/kube_resource_fetcher.rb
+++ lib/kube_resource_fetcher.rb
@@ -8,7 +8,6 @@ class KubeResourceFetcher
   def initialize(repo, commit:, request_id: nil)
     @repo = repo
     @commit = commit
-    @working_dir = 'tmp'
     @request_id = request_id
   end
 
@@ -35,54 +34,12 @@ class KubeResourceFetcher
 
   private
 
-  attr_reader :repo, :working_dir, :commit, :request_id
+  attr_reader :repo, :commit, :request_id
 
   def in_commit
-    in_repo do
-      Git.change_ref commit do
-        Git.pull
-        log_events
-        yield
-      end
+    RepoFetcher.new(repo, commit: commit, request_id: request_id).in_commit do
+      yield
     end
-  rescue IOError => exception
-    if exception.message.start_with? 'fatal: reference is not a tree'
-      raise IOError, "Cannot find commit #{commit} in "\
-                     "github repository #{repo.github}"
-    end
-    raise exception
-  end
-
-  def in_repo
-    Dir.mkdir working_dir unless Dir.exist? working_dir
-    Dir.chdir working_dir do
-      RepoFetcher.new(repo.github).in_repo do
-        yield
-      end
-    end
-  end
-
-  def log_events
-    return if request_id.nil?
-    EventLog.log request_id, ['author', commit_author]
-    EventLog.log request_id, ['insertions', commit_insertions]
-    EventLog.log request_id, ['deletions', commit_deletions]
-  end
-
-  def commit_author
-    Git.current_commit_author
-  end
-
-  def commit_insertions
-    commit_stats[:insertions]
-  end
-
-  def commit_deletions
-    commit_stats[:deletions]
-  end
-
-  def commit_stats
-    @commit_stats ||= Git.current_commit_stats
   end
 
   class << self
--- lib/repo_fetcher.rb
+++ lib/repo_fetcher.rb
@@ -6,36 +6,91 @@ require 'config/log'
 class RepoFetcher
   attr_reader :repo
 
-  def initialize(repo)
+  def initialize(repo, commit:, request_id: nil)
     @repo = repo
+    @commit = commit
+    @request_id = request_id
+    @working_dir = 'tmp'
   end
 
   def pull
-    if File.exist? dir_name
-      Log.debug "repo #{repo} exists -> fetching remote"
-      Dir.chdir(dir_name) do
-        Git.fetch
+    in_working_dir do
+      if File.exist? dir_name
+        Log.debug "repo #{repo.github} exists -> fetching remote"
+        Dir.chdir(dir_name) do
+          Git.fetch
+        end
+      else
+        Log.debug "cloning repo #{repo.github}"
+        Git.clone_github repo
       end
-    else
-      Log.debug "cloning repo #{repo}"
-      Git.clone_github repo
     end
   end
 
-  def in_repo
-    pull
-    Dir.chdir dir_name do
-      Log.debug "Dir.chdir #{dir_name} start"
-      Log.debug Command.run 'pwd'
-      yield
-      Log.debug "Dir.chdir #{dir_name} end"
+  def in_commit
+    in_repo do
+      Git.change_ref commit do
+        Git.pull
+        log_events
+        yield
+      end
     end
-    Log.debug Command.run 'pwd'
+  rescue IOError => exception
+    if exception.message.start_with? 'fatal: reference is not a tree'
+      raise IOError, "Cannot find commit #{commit} in "\
+                     "github repository #{repo.github}"
+    end
+    raise exception
   end
 
   private
 
+  attr_reader :working_dir, :commit, :request_id
+
+  def in_repo
+    in_working_dir do
+      pull
+      Dir.chdir dir_name do
+        Log.debug "Dir.chdir #{dir_name} start"
+        Log.debug Command.run 'pwd'
+        yield
+        Log.debug "Dir.chdir #{dir_name} end"
+      end
+      Log.debug Command.run 'pwd'
+    end
+  end
+
+  def in_working_dir
+    Dir.mkdir working_dir unless Dir.exist? working_dir
+    Dir.chdir working_dir do
+      yield
+    end
+  end
+
   def dir_name
-    repo.split('/').last
+    repo.github.split('/').last
+  end
+
+  def log_events
+    return if request_id.nil?
+    EventLog.log request_id, ['author', commit_author]
+    EventLog.log request_id, ['insertions', commit_insertions]
+    EventLog.log request_id, ['deletions', commit_deletions]
+  end
+
+  def commit_author
+    Git.current_commit_author
+  end
+
+  def commit_insertions
+    commit_stats[:insertions]
+  end
+
+  def commit_deletions
+    commit_stats[:deletions]
+  end
+
+  def commit_stats
+    @commit_stats ||= Git.current_commit_stats
   end
 end
